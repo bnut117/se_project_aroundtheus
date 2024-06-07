13d30dca5c247591436f11d903a931897238c6d4
bretworkman <bretworkman@outlook.com>
2024-06-07 06:09:38 -0400

    final edit

--- scripts/index.js
+++ scripts/index.js
@@ -55,7 +55,7 @@ const cardTemplate =
  *************/
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -88,7 +88,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
   profileDescriptionInput.value = profileDescription.textContent;
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileEditCloseButton.addEventListener("click", closePopup);
