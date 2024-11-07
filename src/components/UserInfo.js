93b4170b670a2569c369cd15d9db002ba2c9273b
bretworkman <bretworkman@outlook.com>
2024-11-07 17:59:45 -0500

    Project 9 User data update

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,18 +1,23 @@
 export default class UserInfo {
-  constructor({ nameSelector, jobSelector }) {
+  constructor({ nameSelector, jobSelector, avatarSelector }) {
     this._nameElement = document.querySelector(nameSelector);
     this._jobElement = document.querySelector(jobSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
+  }
+
+  setUserInfo({ name, job, avatar }) {
+    this._nameElement.textContent = name;
+    this._jobElement.textContent = job;
+    if (this._avatarElement) {
+      this._avatarElement.src = avatar;
+    }
   }
 
   getUserInfo() {
     return {
       name: this._nameElement.textContent,
       job: this._jobElement.textContent,
+      avatar: this._avatarElement ? this._avatarElement.src : "",
     };
-  }
-
-  setUserInfo({ name, job }) {
-    this._nameElement.textContent = name;
-    this._jobElement.textContent = job;
   }
 }
