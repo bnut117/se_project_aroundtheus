b62c6788c330afd1809c2ce2afcb5cd09fc1b769
bretworkman <bretworkman@outlook.com>
2024-11-11 20:18:42 -0500

    Project 9 submission 3

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,9 +6,7 @@ export default class PopupWithForm extends Popup {
     this._handleFormSubmit = handleFormSubmit;
     this._popupForm = this._popupElement.querySelector(".modal__form");
     this._inputList = this._popupForm.querySelectorAll(".modal__input");
-    this._submitButton = this._popupElement.querySelector(
-      ".modal__save-button"
-    );
+    this._submitButton = this._popupElement.querySelector(".modal__button");
     this._defaultButtonText = this._submitButton.textContent;
   }
 
