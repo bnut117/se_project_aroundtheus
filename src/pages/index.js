d2d2df011abc5546eff2213a132b7ce890f3f4bf
bretworkman <bretworkman@outlook.com>
2024-11-15 05:44:23 -0500

    Project 9 submission 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -118,9 +118,9 @@ function handleDeleteCardWithApi(data) {
   deleteCardModal.setSubmitAction(() => {
     deleteCardModal.setIsLoading(true);
     api
-      .deleteCard(data.id)
+      .deleteCard(data._id)
       .then(() => {
-        data.handleDeleteCard();
+        data._handleDeleteCard();
         deleteCardModal.close();
       })
       .catch((err) => console.error(`Error: ${err}`))
